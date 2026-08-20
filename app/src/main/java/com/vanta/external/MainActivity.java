0dddfb225c3f53a0e36ce108c783751f1483fd4a
g3wary <s09987370@gmail.com>
2026-08-20 19:26:07 +0300

    Create MainActivity.java

---
+++ app/src/main/java/com/vanta/external/MainActivity.java
@@ -0,0 +1,28 @@
+package com.vanta.external;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, 123);
+        } else {
+            startOverlay();
+        }
+    }
+
+    private void startOverlay() {
+        startService(new Intent(this, OverlayService.class));
+        finish();
+    }
+}
